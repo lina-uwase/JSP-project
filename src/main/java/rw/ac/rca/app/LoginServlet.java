0dd641638a8852247fd841fc0168144e3fd24a2f
lina-uwase <u.lina250@gmail.com>
2023-03-13 15:19:50 +0200

    initial commit

---
+++ src/main/java/rw/ac/rca/app/LoginServlet.java
@@ -0,0 +1,19 @@
+package rw.ac.rca.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
